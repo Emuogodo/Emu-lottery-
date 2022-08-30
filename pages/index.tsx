7d2a85cf1089d8774e94a3db44b671c93f5632e9
Martin Mwangi <mwangimartin1904@gmail.com>
2022-08-30 22:23:36 +0300

    Login page and show loading when getting contract

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,39 @@
+import { useAddress, useContract } from '@thirdweb-dev/react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import Header from '../components/Header'
+import Login from '../components/Login'
+import PropagateLoader from "react-spinners/PropagateLoader";
 
 const Home: NextPage = () => {
+  const address = useAddress();
+  const { contract, isLoading } = useContract(
+    process.env.NEXT_PUBLIC_LOTTERY_CONTRACT_ADDRESS
+  );
+
+  console.log(address)
+
+
+  if (isLoading) return (
+    <div className='bg-[#091B18] min-h-screen flex flex-col items-center justify-center'>
+      <div className='flex items-center space-x-2 mb-10'>
+        <img className='rounded-full h-20 w-20' src="https://i.imgur.com/4h7mAu7.png" alt="" />
+        <h1 className='text-lg text-white font-bold'>Loading the PAPAFAM DRAW</h1>
+      </div>
+      <PropagateLoader color='white' size={30} />
+    </div>
+  )
+
+  if (!address) return <Login />
+
   return (
-    <div className="">
+    <div className="bg-[#091B18] min-h-screen flex flex-col">
       <Head>
         <title>My Lottery App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Lets build an awesome lottery WEB3 Dapp</h1>
-
+      <Header />
     </div>
   )
 }
